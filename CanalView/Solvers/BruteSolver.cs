bb402a9bafe6d3c22948a38c72ce4cd3ed12df0b
Yonatan Atlas <atlasyonatan@gmail.com>
2021-07-26 20:57:09 +0300

    changed brute solver to generator pattern

--- CanalView/Solvers/BruteSolver.cs
+++ CanalView/Solvers/BruteSolver.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,60 +6,32 @@ namespace CanalView.Solvers
 {
     public class BruteSolver : ISolver
     {
-        public IEnumerable<Cell[,]> Solve(Cell[,] board) => new EnumerableObject(board);
-        private class EnumerableObject : IEnumerable<Cell[,]>
+        private static readonly Cell[] FillOptions = new Cell[] { Cell.Full, Cell.Empty };
+        public IEnumerable<Cell[,]> Solve(Cell[,] board)
         {
-            private readonly Cell[,] _board;
-            public EnumerableObject(Cell[,] board) => _board = (Cell[,])board.Clone();
-            public IEnumerator<Cell[,]> GetEnumerator() => new EnumeratorObject(_board);
-            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-        }
-        private class EnumeratorObject : IEnumerator<Cell[,]>
-        {
-            private readonly Cell[,] _board;
-            private readonly (int X, int Y)[] _unknowns;
-            private readonly Cell[] _fillOptions = new Cell[] { Cell.Full, Cell.Empty };
-            private int _index = 0;
-
-            public EnumeratorObject(Cell[,] board)
-            {
-                _board = board.Copy();
-                _unknowns = _board.GetSpots()
-                    .Where(s => _board[s.X, s.Y] == Cell.Unkown)
+            board = board.Copy();
+            var i = 0;
+            var unknowns = board.GetSpots()
+                    .Where(s => board[s.X, s.Y] == Cell.Unkown)
                     .ToArray();
-            }
-
-            public bool MoveNext()
+            while (i >= 0)
             {
-                while (_index >= 0)
+                var (x, y) = unknowns[i];
+                var cell = board[x, y];
+                int nextGuessIndex = cell == Cell.Unkown ? 0 : Array.IndexOf(FillOptions, cell) + 1;
+                if (nextGuessIndex >= FillOptions.Length)
                 {
-                    var (X, Y) = _unknowns[_index];
-                    var cell = _board[X, Y];
-                    int nextGuessIndex = cell == Cell.Unkown ? 0 : Array.IndexOf(_fillOptions, cell) + 1;
-                    if (nextGuessIndex >= _fillOptions.Length)
-                    {
-                        _board[X, Y] = Cell.Unkown;
-                        _index--;
-                        continue;
-                    }
-                    _board[X, Y] = _fillOptions[nextGuessIndex];
-                    if (_board.LegalSquare(X, Y) && _board.LegalNumbers(X, Y))
-                        if (_index < _unknowns.Length - 1)
-                            _index++;
-                        else if ((_board[X, Y] == Cell.Full && _board.LegalPath(X, Y)) || (_board[X, Y] != Cell.Full && _board.LegalPath()))
-                        {
-                            Current = _board.Copy();
-                            return true;
-                        }
+                    board[x, y] = Cell.Unkown;
+                    i--;
+                    continue;
                 }
-                return false;
+                board[x, y] = FillOptions[nextGuessIndex];
+                if (Legality.LegalSquare(board, x, y) && Legality.LegalNumbers(board, x, y))
+                    if (i < unknowns.Length - 1)
+                        i++;
+                    else if ((board[x, y] == Cell.Full && Legality.LegalPath(board, x, y)) || (board[x, y] != Cell.Full && Legality.LegalPath(board)))
+                        yield return board.Copy();
             }
-
-            public void Reset() => throw new NotSupportedException();
-            public IEnumerator GetEnumerator() => this;
-            public void Dispose() { }
-            public Cell[,] Current { get; private set; } = null;
-            object IEnumerator.Current => Current;
         }
     }
 }
